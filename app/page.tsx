6d0019e56ac6470c77bc1d2543c30212c0b3b5a5
xkenn <ervin.gonzales.inno@gmail.com>
2024-09-29 23:50:59 +0800

    refactor: add side panel to main

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import ParticlesBackground from './components/ParticlesBackground';
 import MainPanel from './components/MainPanel';
 import { Quattrocento, Roboto } from 'next/font/google';
+import SidePanel from './components/SidePanel';
 
 // Correct variable name to reflect the font
 const quattrocento = Quattrocento({
@@ -9,14 +10,13 @@ const quattrocento = Quattrocento({
   weight: '700',
 });
 
-
 export default function Home() {
   return (
     <div className="w-full flex items-center justify-center h-screen bg-black">
       <ParticlesBackground />
       <div className="flex flex-row items-center justify-between container w-full h-[80%] space-x-4 z-10">
-        <div className="w-[26%] flex items-center h-full justify-center bg-slate-300 rounded-lg">
-          <MainPanel />
+        <div className="w-[26%] flex items-center h-full justify-center  overflow-hidden">
+          <SidePanel />
         </div>
         <div className="w-[72%] flex items-center h-full justify-center bg-yellow-800 rounded-lg">
           <MainPanel />
