414211f107ae8e2be3de9049f09e2b4e626da790
xkenn <ervin.gonzales.inno@gmail.com>
2024-09-29 22:43:02 +0800

    feat: add font and particle component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,27 @@
-import Image from "next/image";
+import ParticlesBackground from './components/ParticlesBackground';
+import MainPanel from './components/MainPanel';
+import { Quattrocento, Roboto } from 'next/font/google';
+
+// Correct variable name to reflect the font
+const quattrocento = Quattrocento({
+  subsets: ['latin'],
+  variable: '--font-quattrocento', // Optional: Define a CSS variable for font
+  weight: '700',
+});
+
 
 export default function Home() {
   return (
-    <div className="w-full flex items-center justify-center h-screen bg-slate-400">
-      Hello
+    <div className="w-full flex items-center justify-center h-screen bg-black">
+      <ParticlesBackground />
+      <div className="flex flex-row items-center justify-between container w-full h-[80%] space-x-4 z-10">
+        <div className="w-[26%] flex items-center h-full justify-center bg-slate-300 rounded-lg">
+          <MainPanel />
+        </div>
+        <div className="w-[72%] flex items-center h-full justify-center bg-yellow-800 rounded-lg">
+          <MainPanel />
+        </div>
+      </div>
     </div>
   );
 }
