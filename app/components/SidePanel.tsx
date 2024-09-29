8582ebcee7a4506dd2b711c1ac3cc82fd532cb78
xkenn <ervin.gonzales.inno@gmail.com>
2024-09-29 20:34:35 +0800

    feat: add side panel

---
+++ app/components/SidePanel.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SidePanel = () => {
+  return (
+    <div className="w-[200px] bg-gray-700 border border-black h-full">
+      SidePanel
+    </div>
+  );
+};
+
+export default SidePanel;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
-import Image from "next/image";
+import Image from 'next/image';
+import SidePanel from './components/SidePanel';
 
 export default function Home() {
   return (
-    <div className="w-full flex items-center justify-center h-screen bg-slate-400">
-      Hello
-    </div>
+    <section className=" flex items-center justify-center bg-white w-screen h-screen p-10">
+      <div className="w-full flex-row items-center justify-center h-screen bg-slate-400 container p-10">
+        <SidePanel />
+      </div>
+    </section>
   );
 }
