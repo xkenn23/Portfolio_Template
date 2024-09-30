4f8e91336560adc2f9cfe580ff9b7881fe45c4b3
xkenn <ervin.gonzales.inno@gmail.com>
2024-09-30 22:04:48 +0800

    feat: add compoent for main panel and work on side panel

--- app/components/SidePanel.tsx
+++ app/components/SidePanel.tsx
@@ -1,31 +1,41 @@
 import React from 'react';
 import { NAVIGATION, NavigationItem } from '../constants/Content';
+import Image from 'next/image';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const SidePanel = () => {
   return (
     <div
       className="
-        h-full w-full bg-purple-500 rounded-md bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-30 border 
-        flex flex-col items-center justify-center overflow-hidden"
+      h-full w-full bg-purple-500 rounded-md bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-30 border 
+      flex flex-col items-center justify-center overflow-hidden"
     >
-      <div>Photo</div>
-      <div className="flex flex-col items-center justify-center">
+      <div className="flex flex-col items-center justify-center space-y-3 ">
+        <Image
+          src="/profile_image.png"
+          width={200}
+          height={200}
+          alt="Picture of the author"
+          className="rounded-full overflow-hidden"
+        />
+        <text className="font-quattrocento text-2xl ">Kenn</text>
+      </div>
+      <div className="flex flex-col items-center justify-center ">
         {/* <div className="text-lg font-bold">Navigation</div> */}
-        <div className="mt-6 flex flex-col items-center font-quattrocento">
+        <div className="mt-6 flex flex-col items-center font-quattrocento space-y-2">
           {NAVIGATION.map((item: NavigationItem) => (
             <div
               key={item.label}
-              className=" hover:text-blue-700 cursor-pointer mt-2 text-white font-quattrocento font-bold text-lg"
+              className=" hover:text-blue-700 cursor-pointer text-white font-quattrocento font-bold text-xl"
             >
               {item.label}
             </div>
           ))}
         </div>
-        <div className="mt-6 flex flex-col items-center text-white font-quattrocento font-bold text-lg">
-          Short Intro / Title
-        </div>
-
-        <div className="mt-6 flex flex-row items-center">Social Icons</div>
+      </div>
+      <div className=" flex flex-col items-center text-white font-quattrocento text-lg text-center space-y-3 mt-6">
+        <text className="text-2xl">Web Developer</text>
+        <div className="mt-6 flex flex-row items-center"></div>
       </div>
     </div>
   );
